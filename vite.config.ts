4a1829283ccdaeb507204ae7604593d6a15c6040
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-29 11:28:36 +0600

    123

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import fs from "fs"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  async server(){
+    const key = await fs.readFile(__dirname + '/ssl/ca.key')
+    const cert = await fs.readFile(__dirname + '/ssl/ca.crt')
+    return { https: { key, cert } }
+  },
   plugins: [react()],
 })
