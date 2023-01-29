777f4a863d25a619a4fe5195b51879ff2131ff89
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-29 11:50:31 +0600

    123

--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -1,9 +1,7 @@
 export default function() {
     render() {
         return (
-            <div style="position: absolute; top:0; left:0; width:100%; height:100%;">
-                123
-            </div>
+            "123"
         )
     }
 }
