696915e6190f6affa1613dce3ac7b038da3123a2
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-29 11:46:45 +0600

    123

---
+++ src/components/nav/bar.tsx
@@ -0,0 +1,13 @@
+import Button from "./Button"
+
+export default function () {
+    render() {
+        return (
+            <nav>
+                <Button />
+                <Button />
+                <Button />
+            </nav>
+        )
+    }
+}
---
+++ src/components/screen.tsx
@@ -0,0 +1,9 @@
+export default function() {
+    render() {
+        return (
+            <div style="position: absolute; top:0; left:0; width:100%; height:100%;">
+                123
+            </div>
+        )
+    }
+}
