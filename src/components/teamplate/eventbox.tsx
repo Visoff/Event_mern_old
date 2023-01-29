0d0802e67dc05082d782da32ecf7fed87188275d
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-29 19:02:39 +0600

    123

--- src/components/teamplate/eventbox.tsx
+++ src/components/teamplate/eventbox.tsx
@@ -8,8 +8,8 @@ interface Props {
 export default function ({name, description}:Props) {
     return (
         <div className="event-box">
-            <span>{name}</span>
-            <span>{description}</span>
+            <h1>{name}</h1>
+            <p>{description}</p>
             <button>Подробнее</button>
         </div>
     )
