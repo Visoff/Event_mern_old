61ba65a470204531c5719335ee319702ece78350
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-29 19:15:14 +0600

    123

--- src/components/frames/home/index.tsx
+++ src/components/frames/home/index.tsx
@@ -3,7 +3,7 @@ import EventBox from "./../../teamplate/eventbox"
 export default function () {
     return (
         <div className="event-box-parent">
-            <EventBox name="CityHeroes" description="description" />
+            <EventBox name="CityHeroes" description="yooo it works" />
             <EventBox name="CityHeroes" description="description" />
             <EventBox name="CityHeroes" description="description" />
         </div>
