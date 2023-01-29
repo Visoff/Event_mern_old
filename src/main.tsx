2ae68fd1f330f650be74c26a83f4dc77542f3928
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-29 11:48:06 +0600

    123

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React, {FC} from 'react'
 import ReactDOM from 'react-dom/client'
-import fullscreen from './components/screen'
+import Window from './components/screen'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <fullscreen />
+    <Window />
   </React.StrictMode>,
 )
