3c43d9e4cd904405cb7ef326db7ad285e3857b5f
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-29 18:00:03 +0600

    123

--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -1,10 +1,12 @@
 import NavBar from "./nav/bar"
+import Frame from "./frames/father"
 
 import "./screen.css"
 
 export default function() {
     return (
         <div className="screen">
+            <Frame />
             <NavBar />
         </div>
     )
--- src/onload.ts
+++
@@ -1,8 +0,0 @@
-export default function() {
-    document.querySelectorAll("a").forEach(el => {
-        el.onclick = function(event) {
-            event.preventDefault()
-            console.log(123)
-        }
-    })
-}
