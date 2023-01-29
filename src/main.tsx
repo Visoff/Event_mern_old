dc5dac7bf574dc59daedfa781acb372a720e37ba
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-29 18:30:48 +0600

    123

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import Window from './components/screen'
 import './index.css'
 
 const currentFrame = React.createContext()
-console.log(currentFrame)
+console.log(React.useContext(currentFrame))
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
