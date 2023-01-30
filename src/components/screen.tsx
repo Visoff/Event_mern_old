f0c35aa4e9de3943c59a7fbfca30b911fb30605f
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-30 20:47:24 +0600

    123

--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -3,7 +3,7 @@ import Frame from "./frames/father"
 
 import "./screen.css"
 
-export default function() {
+export default function screen() {
     return (
         <div className="screen">
             <Frame />
