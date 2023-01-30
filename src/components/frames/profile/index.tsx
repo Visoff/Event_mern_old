9efef190f7d8f25fbe5bafe804ad9cf6fa8e3e6e
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-30 20:55:05 +0600

    123

--- src/components/frames/profile/index.tsx
+++ src/components/frames/profile/index.tsx
@@ -3,7 +3,13 @@ import "./index.css"
 export default function ProfileFrame() {
     return (
         <div className="profile-frame">
-            <div className="icon"></div>
+            <div className="row">
+                <div className="icon"></div>
+                <div className="name-parent">
+                    <div className="name">Ilya</div>
+                    <div className="description">I suck and please help me (nvm i have chat gpt)</div>
+                </div>
+            </div>
         </div>
     )
 }
