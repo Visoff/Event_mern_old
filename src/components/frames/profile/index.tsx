6bea009b29b3e7dcd9e6bf6f33e7bde7b271c2a1
sashynkaDyrachek <ikalinin667@gmail.com>
2023-01-30 21:17:50 +0600

    123

--- src/components/frames/profile/index.tsx
+++ src/components/frames/profile/index.tsx
@@ -14,8 +14,12 @@ export default function ProfileFrame() {
             <div className="horizontal-list event-list">
                 <EventBox name="CityHeroes" description="came first" />
                 <EventBox name="CityHeroes" description="came first" />
+                <EventBox name="CityHeroes" description="came first" />
+                <EventBox name="CityHeroes" description="came first" />
             </div>
             <div className="horizontal-list team-list">
+                <EventBox color="secondary" name="CityHeroes" description="came first" />
+                <EventBox color="secondary" name="CityHeroes" description="came first" />
                 <EventBox color="secondary" name="CityHeroes" description="came first" />
                 <EventBox color="secondary" name="CityHeroes" description="came first" />
             </div>
