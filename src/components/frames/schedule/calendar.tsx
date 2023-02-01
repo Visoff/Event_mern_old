a4f0f58e1e8ddf3a37d20774b520adcbb4a83974
sashynkaDyrachek <ikalinin667@gmail.com>
2023-02-01 15:20:59 +0600

    123

--- src/components/frames/schedule/calendar.tsx
+++ src/components/frames/schedule/calendar.tsx
@@ -1,13 +1,24 @@
 export default function calendar() {
     
-    const calendar = [
-        <div>123</div>,
-        <div>456</div>
-    ]
+    const res = []
+
+    const current_date = new Date()
+    const current_year = current_date.getFullYear()
+    const current_month = current_date.getMonth()
+    const last_day = new Date(current_year, current_month+1, 0)
+    const month_length = last_day.getDate()
+
+    var cursor = new Date(current_year, current_month, 1);
+
+    for (var i = 1; i < month_length; i++) {
+        cursor.setDate(i)
+        if (cursor.getDay() == 1) {res.push(<br></br>)}
+        res.push(<div>i</div>)
+    }
 
     return (
-        <table>
-            {calendar}
-        </table>
+        <div>
+            {res}
+        </div>
     );
 }
