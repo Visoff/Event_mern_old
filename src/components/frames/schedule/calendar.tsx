ed85f393be9a60258ff20c094e46e27021f00d3f
sashynkaDyrachek <ikalinin667@gmail.com>
2023-02-01 16:21:09 +0600

    123

--- src/components/frames/schedule/calendar.tsx
+++ src/components/frames/schedule/calendar.tsx
@@ -1,37 +1,83 @@
+import React, { useState } from 'react';
+
 import "./calendar.css"
 
-export default function Calendar() {
-    
-    const res = []
+const Calendar = () => {
+  const [date, setDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(null);
 
-    const current_date = new Date()
-    const current_year = current_date.getFullYear()
-    const current_month = current_date.getMonth()
-    const last_day = new Date(current_year, current_month+1, 0)
-    const month_length = last_day.getDate()
+  const handlePrevMonth = () => {
+    setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1));
+  };
 
-    var cursor =  new Date();
-    for (var i = 1; i <= month_length; i++) {
-        cursor.setDate(i)
-        res.push(<div key={i}>{i}</div>)
+  const handleNextMonth = () => {
+    setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1));
+  };
+
+  const renderDays = () => {
+    const days = [];
+    const dateCopy = new Date(date);
+    dateCopy.setDate(1);
+    const offset = dateCopy.getDay();
+    dateCopy.setDate(0 - offset);
+
+    for (let i = 0; i < 42; i++) {
+      dateCopy.setDate(dateCopy.getDate() + 1);
+      const currentDay = dateCopy.getDate();
+      const currentMonth = dateCopy.getMonth();
+
+      days.push(
+        <td key={i} className={`calendar-day ${currentMonth === date.getMonth() ? 'current-month' : 'other-month'} ${selectedDate && selectedDate.getDate() === currentDay && selectedDate.getMonth() === currentMonth ? 'selected' : ''}`} onClick={() => setSelectedDate(new Date(dateCopy))}>
+          {currentDay}
+        </td>
+      );
     }
-    var i = 1
-    cursor = new Date(current_year, current_month+1, 1)
-    while (cursor.getDay() != 0) {
-        cursor.setDate(++i)
-        res.push(<div key={i}>{i}</div>)
-    }
-    cursor = new Date(current_year, current_month, 0)
-    var i = cursor.getDate()+1
-    while (cursor.getDay() != 1) {
-        cursor.setDate(--i)
-        res.unshift(<div key={i}>{i}</div>)
-    }
 
+    return days;
+  };
 
-    return (
-        <div className="calendar">
-            {res}
-        </div>
-    );
-}
+  return (
+    <div className="calendar">
+      <table>
+        <thead>
+          <tr>
+            <th onClick={handlePrevMonth}>{'<'}</th>
+            <th colSpan="5">{`${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()}`}</th>
+            <th onClick={handleNextMonth}>{'>'}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Sun</th>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thu</th>
+            <th>Fri</th>
+            <th>Sat</th>
+          </tr>
+          <tr>
+            {renderDays().slice(0, 7)}
+          </tr>
+          <tr>
+            {renderDays().slice(7, 14)}
+          </tr>
+          <tr>
+            {renderDays().slice(14, 21)}
+          </tr>
+          <tr>
+            {renderDays().slice(21, 28)}
+          </tr>
+          <tr>
+            {renderDays().slice(28, 35)}
+          </tr>
+          <tr>
+            {renderDays().slice(35, 42)}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Calendar;
