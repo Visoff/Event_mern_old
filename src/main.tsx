0d491b0b5b56b8f6b9f7f517cc975760d8bca6cb
sashynkaDyrachek <ikalinin667@gmail.com>
2023-02-01 20:46:26 +0600

    123

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
-import React, {FC, useContext} from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from "axios"
+
 import Window from './components/screen'
 import './index.css'
 
 declare global {
   interface Window {
-    setframe:Function
+    setframe:Function,
+    user:{
+      data:Object,
+      setData:Function
+    }
+  }
+}
+
+{
+  const [data, setData] = useState({id:-1})
+  window.user = {data, setData}
+  var id;
+  if ((id = localStorage.getItem("user_id")) != undefined) {
+    (async (id) => {
+      var data = await axios.post("https://visoff.ru/api/db/user/getBy/id", {
+        data:{id}
+      })
+      console.log(data)
+    })(id)
   }
 }
 
