73ed56f180d0966d65cfc21b4fb32dbc83c303b9
sashynkaDyrachek <ikalinin667@gmail.com>
2023-02-06 20:51:19 +0600

    123

--- src/components/teamplate/eventbox.tsx
+++ src/components/teamplate/eventbox.tsx
@@ -12,10 +12,33 @@ export default function EventBox({name, description, color}:Props) {
         classgen+=" secondary-color"
     }
     return (
+        /*
         <div className={classgen}>
             <h1>{name}</h1>
             <p>{description}</p>
             <button>Подробнее</button>
+        </div>*/
+        
+        <div className={classgen}>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <div className="event-info">
+                <div className="event-info-item">
+                    <p className="event-info-label">Date:</p>
+                    <p className="event-info-value">{date}</p>
+                </div>
+                <div className="event-info-item">
+                    <p className="event-info-label">Tags:</p>
+                    <div className="event-info-value event-tags">
+                        {tags.map((tag, index) => <span key={index}>{tag}</span>)}
+                    </div>
+                </div>
+                <div className="event-info-item">
+                    <p className="event-info-label">Registrations:</p>
+                    <p className="event-info-value">50</p>
+                </div>
+            </div>
+            <button>Зарегистрироваться</button>
         </div>
     )
 }
