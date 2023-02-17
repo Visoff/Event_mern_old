fa544c18e67c0d6cf6baf6ee5f52579e11a8a061
Ilya <ikalinin667@gmail.com>
2023-02-17 20:14:39 +0600

    123

--- src/components/frames/registration/index.tsx
+++ src/components/frames/registration/index.tsx
@@ -5,12 +5,19 @@ import style from "./index.module.css"
 export default function Registration() {
     const [mode, setmode] = useState("Registration")
     const [stage, setstage] = useState(0)
-    window.setstage = setstage
     if (mode == "Registration") {
         return (
             <div className={style.main}>
                 <div className={style.form+(stage == 0 ? " "+style.current : "")}>
-                    hello
+                    <form onSubmit={(e) => {e.preventDefault()}}>
+                        <p>Почта</p>
+                        <input type="text" />
+                        <p>Пароль</p>
+                        <input type="text" />
+                        <p>Подтверждение паролья</p>
+                        <input type="text" />
+                        <button type="submit">Подтвердить</button>
+                    </form>
                 </div>
                 <div className={style.form+(stage == 1 ? " "+style.current : "")}>
                     o/
