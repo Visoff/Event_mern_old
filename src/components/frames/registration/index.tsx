6f8e7b683fd8cd8047281332b30b41abefc9a35e
Ilya <ikalinin667@gmail.com>
2023-02-17 20:11:31 +0600

    123

--- src/components/frames/registration/index.tsx
+++ src/components/frames/registration/index.tsx
@@ -4,13 +4,14 @@ import style from "./index.module.css"
 
 export default function Registration() {
     const [mode, setmode] = useState("Registration")
+    const [stage, setstage] = useState(0)
     if (mode == "Registration") {
         return (
             <div className={style.main}>
-                <div className={style.form+" "+style.current}>
+                <div className={style.form+(stage == 0 ? " "+style.current : "")}>
                     hello
                 </div>
-                <div className={style.form}>
+                <div className={style.form+(stage == 1 ? " "+style.current : "")}>
                     o/
                 </div>
             </div>
