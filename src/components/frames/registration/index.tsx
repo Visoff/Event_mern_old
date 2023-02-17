c3818bb88f823b02f256267c05cbfed6c352451d
Ilya <ikalinin667@gmail.com>
2023-02-17 21:26:06 +0600

    123

--- src/components/frames/registration/index.tsx
+++ src/components/frames/registration/index.tsx
@@ -30,6 +30,23 @@ export default function Registration() {
                         <button type="submit">Дальше</button>
                     </form>
                 </div>
+                <button className={style.bottom}>Уже есть аккаунт? Авторизоваться</button>
+            </div>
+        )
+    } else if (mode == "Login") {
+        return (
+            <div className={style.main}>
+                <div className={style.form+" "+style.current}>
+                    <form onSubmit={(e) => {e.preventDefault(); setstage(1)}}>
+                        <h1>Авторизация</h1>
+                        <p>Почта</p>
+                        <input type="email" placeholder="example@mail.ru" />
+                        <p>Пароль</p>
+                        <input type="password" placeholder="***********" />
+                        <button type="submit">Войти</button>
+                    </form>
+                </div>
+                <button className={style.bottom}>Ещё нет аккаунта? Зарегистрироваться</button>
             </div>
         )
     }
