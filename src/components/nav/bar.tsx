d955690ffbff84db9b5cbfb169977d92b492cdb4
Ilya <ikalinin667@gmail.com>
2023-02-17 19:12:52 +0600

    123

--- src/components/nav/bar.tsx
+++ src/components/nav/bar.tsx
@@ -8,6 +8,7 @@ export default function NavBar() {
             <Button href="Home" name="Главная" />
             <Button href="Schedule" name="Календарь" />
             <Button href="Profile" name="Профиль" />
+            <Button href="Profile" name="Твои" />
         </nav>
     )
 }
