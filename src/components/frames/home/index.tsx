4969dee1a395cf91be475a59f2e6e9b9894f3a9e
Ilya <ikalinin667@gmail.com>
2023-02-19 21:07:41 +0600

    123

--- src/components/frames/home/index.tsx
+++ src/components/frames/home/index.tsx
@@ -1,10 +1,19 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import EventInDetails from "../../teamplate/EventInDetails"
 
 export default function HomeFrame() {
+    const [events, setEvents] = useState([])
+    useEffect(() => {
+        if (events.length == 0) {
+            axios.post("https://visoff.ru/api/db/event/find", {}).then(data => {
+                setEvents(data.data)
+            })
+        }
+    }, [])
     return (
         <div className="event-box-parent">
-            <EventInDetails name="CityHeroes" description="yooo it works" date="12-12-12" tags={["Хакатон", "CityHeroes", "7bits"]} />
-            <EventInDetails name="CityHeroes" description="yooo it works" date="12-12-12" tags={["12", "13", "14"]} />
+            {events.map(el => {return <EventInDetails name={el.name??"Название"} description={el.description??"Описание"} date={el.date??"12.12.22"} tags={el.tags??[]} />})}
             <EventInDetails name="CityHeroes" description="yooo it works" date="12-12-12" tags={["12", "13", "14"]} />
         </div>
     )
