d76cdbb243e4fadc08dd310b75713953315f750a
Ilya <ikalinin667@gmail.com>
2023-02-19 12:49:45 +0600

    123

--- src/components/frames/registration/index.tsx
+++ src/components/frames/registration/index.tsx
@@ -13,7 +13,7 @@ function register(regdata:Object) {
 }
 function login(regdata:Object) {
     axios.post("https://visoff.ru/api/db/user/getBy/emailPass", {email:(regdata.email), password:(regdata.password)}).then(data => {
-        localStorage.setItem("user_id", data.data.id)
+        console.log("user_id", data.data)
     })
 }
 
