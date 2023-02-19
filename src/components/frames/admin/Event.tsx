086fce389ef05db2adf8fb578b516c023c25dac0
Ilya <ikalinin667@gmail.com>
2023-02-19 20:00:05 +0600

    123

--- src/components/frames/admin/Event.tsx
+++ src/components/frames/admin/Event.tsx
@@ -1,6 +1,13 @@
+import axios from "axios"
 import { useState } from "react"
 
 import style from "./index.module.css"
+
+function update(event) {
+    axios.post("https://visoff.ru/api/db/event/updateOne", event).then((data) => {
+        if (data.data == "ok") {console.log("updated")} else {console.log("moron")}
+    })
+}
 
 export default function event({el}:{el:{}}) {
     const [active, setActive] = useState(false)
@@ -8,7 +15,7 @@ export default function event({el}:{el:{}}) {
     return (
         <div className={style.event+" "+(active ? style.active : "")} onClick={(e) => {setActive(true)}}>
             <h1 className={style.name} contentEditable="true" onChange={(e) => {setEvent({...event, name:(e.target as HTMLElement).innerText})}}>{event.name??"Название"}</h1>
-            <form onSubmit={e => {e.preventDefault(); setActive(false); console.log(event)}}>
+            <form onSubmit={e => {e.preventDefault(); setActive(false); update(event)}}>
                 <p>Описание: <input type="text" onChange={e => {setEvent({...event, description:e.target.value})}} value={event.description} /></p>
                 <button className={style.confirm}>Сохранить</button>
             </form>
