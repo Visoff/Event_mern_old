2f546df6127e9d16cb8b138be5e4121ec267a5db
Ilya <ikalinin667@gmail.com>
2023-02-19 21:12:25 +0600

    123

--- src/components/frames/home/index.tsx
+++ src/components/frames/home/index.tsx
@@ -8,14 +8,12 @@ export default function HomeFrame() {
         if (events.length == 0) {
             axios.post("https://visoff.ru/api/db/event/find", {}).then(data => {
                 setEvents(data.data)
-                console.log(data.data)
             })
         }
     }, [])
     return (
         <div className="event-box-parent">
             {events.map(el => {return <EventInDetails key={el._id} name={el.name??"Название"} description={el.description??"Описание"} date={el.date??"12.12.22"} tags={el.tags??[]} />})}
-            <EventInDetails name="CityHeroes" description="yooo it works" date="12-12-12" tags={["12", "13", "14"]} />
         </div>
     )
 }
