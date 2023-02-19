bfb0060b5acf5566e867e8c907b4920198ea9c12
Ilya <ikalinin667@gmail.com>
2023-02-19 16:37:18 +0600

    123

--- src/components/frames/home/index.tsx
+++ src/components/frames/home/index.tsx
@@ -3,7 +3,7 @@ import EventInDetails from "../../teamplate/EventInDetails"
 export default function HomeFrame() {
     return (
         <div className="event-box-parent">
-            <EventInDetails name="CityHeroes" description="yooo it works" date="12-12-12" tags={["12", "13", "14"]} />
+            <EventInDetails name="CityHeroes" description="yooo it works" date="12-12-12" tags={["Хакатон", "CityHeroes", "7bits"]} />
             <EventInDetails name="CityHeroes" description="yooo it works" date="12-12-12" tags={["12", "13", "14"]} />
             <EventInDetails name="CityHeroes" description="yooo it works" date="12-12-12" tags={["12", "13", "14"]} />
         </div>
