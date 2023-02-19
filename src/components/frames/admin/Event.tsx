4b4f841af03bbcb004297d8c78b374d6b5199905
Ilya <ikalinin667@gmail.com>
2023-02-19 19:50:48 +0600

    123

--- src/components/frames/admin/Event.tsx
+++ src/components/frames/admin/Event.tsx
@@ -7,7 +7,7 @@ export default function event(el:{_id:string, name?:string, description?:string}
     const [event, setEvent] = useState(el)
     return (
         <div className={style.event+" "+(active ? style.active : "")} onClick={(e) => {setActive(true)}}>
-            <h1 className={style.name} contentEditable="true" onChange={(e) => {setEvent({...event, name:(e.target as HTMLElement).innerText})}}>{event.name??"Название"}</h1>
+            <h1 className={style.name} contentEditable="true" onChange={(e) => {console.log({...event, name:(e.target as HTMLElement).innerText})}}>{event.name??"Название"}</h1>
             <form onSubmit={e => {e.preventDefault(); setActive(false);}}>
                 <p>Описание: <input type="text" onChange={e => {console.log({...event, description:e.target.value})}} value={event.description} /></p>
                 <button className={style.confirm}>Сохранить</button>
