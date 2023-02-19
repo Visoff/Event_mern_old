707b864f0244cb58b210658587750c6066f83ea4
Ilya <ikalinin667@gmail.com>
2023-02-19 18:09:40 +0600

    123

--- src/components/frames/admin/index.tsx
+++ src/components/frames/admin/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 import style from "./index.module.css"
 
+function update() {
+    axios.post("https://visoff.ru/api/db/event/getBy/admin", {id:window.user.data._id})
+}
+
 export default function AdminFrame() {
+    const [events, setEvents] = useState([])
+    useEffect(update)
     return (
         <div className={style.main}>
-            <button className={style.add} onClick={(e) => {console.log(123)}}>Добавить</button>
+            {events.map(el => {return <div>{el.name}</div>})}
+            <button className={style.add} onClick={(e) => {
+                axios.post("https://visoff.ru/api/db/event/create", {admins:[window.user.data._id]})
+            }}>Добавить</button>
         </div>
     )
 }
