8b2b08e8bc8c24d2a2d8c47b173f7321c515e54a
Ilya <ikalinin667@gmail.com>
2023-02-21 21:29:16 +0600

    123

--- src/components/frames/schedule/calendar.tsx
+++ src/components/frames/schedule/calendar.tsx
@@ -18,7 +18,7 @@ const Calendar = () => {
     const days = [];
     const dateCopy = new Date(date);
     dateCopy.setDate(1);
-    const offset = (dateCopy.getDay() + 5)%7;
+    const offset = (dateCopy.getDay())%7;
     dateCopy.setDate(1 - offset);
   
     function click(dateCopy: string) {
