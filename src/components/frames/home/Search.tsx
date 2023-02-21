d1438dfa1aa3742c9f1c0dd70cfcc0843c49f2ff
Ilya <ikalinin667@gmail.com>
2023-02-21 21:03:26 +0600

    123

--- src/components/frames/home/Search.tsx
+++ src/components/frames/home/Search.tsx
@@ -3,14 +3,14 @@ import style from "./Search.module.css"
 
 
 export default function Search() {
-    const [timer, settimer] = useState(null)
+    const [timer, settimer] = useState<number>(NaN)
 
-    function change(e) {
+    function change(e: React.ChangeEvent<HTMLInputElement>) {
         clearTimeout(timer)
         var timeout = setTimeout(() => {
             console.log(123)
         }, 1000)
-        settimer(timeout)
+        settimer(timeout as any)
     }
     return (
         <div className={style.main}>
