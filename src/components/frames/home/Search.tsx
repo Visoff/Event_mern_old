7cd4bfc2aae9484126a78a0234f70c3f2e0df121
Ilya <ikalinin667@gmail.com>
2023-02-21 16:47:57 +0600

    123

--- src/components/frames/home/Search.tsx
+++ src/components/frames/home/Search.tsx
@@ -4,6 +4,7 @@ export default function Search() {
     return (
         <div className={style.main}>
             <input type="text" />
+            <button className={style.more}>...</button>
         </div>
     )
 }
