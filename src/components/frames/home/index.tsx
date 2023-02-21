0c06b1c9c4a93baa786acc6d1457e93455703e6a
Ilya <ikalinin667@gmail.com>
2023-02-21 16:27:03 +0600

    123

--- src/components/frames/home/index.tsx
+++ src/components/frames/home/index.tsx
@@ -13,7 +13,7 @@ export default function HomeFrame() {
     }, [])
     return (
         <div className="event-box-parent">
-            {events.map(el => {return <EventInDetails key={el._id} name={el.name??"Название"} description={el.description??"Описание"} date={el.date??"12.12.22"} tags={el.tags??[]} />})}
+            {events.map((el:{_id:string, name:string, description:string, date:string, tags:string[]}) => {return <EventInDetails key={el._id} name={el.name??"Название"} description={el.description??"Описание"} date={el.date??"12.12.22"} tags={el.tags??[]} />})}
         </div>
     )
 }
