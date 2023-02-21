84c50e286d7f7470e8d76ee85958c3df683df351
Ilya <ikalinin667@gmail.com>
2023-02-21 16:50:57 +0600

    123

--- src/components/frames/home/Search.tsx
+++ src/components/frames/home/Search.tsx
@@ -1,9 +1,13 @@
 import style from "./Search.module.css"
 
+function change(e) {
+    console.log(e)
+}
+
 export default function Search() {
     return (
         <div className={style.main}>
-            <input type="text" placeholder="Искать"/>
+            <input type="text" placeholder="Искать" onChange={change}/>
             <button className={style.more}>...</button>
         </div>
     )
