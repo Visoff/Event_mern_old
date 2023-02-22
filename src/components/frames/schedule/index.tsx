0f037d3b228f387ef763bf17b0480a2dbc1e9f82
Ilya <ikalinin667@gmail.com>
2023-02-22 20:33:25 +0600

    123

--- src/components/frames/schedule/index.tsx
+++ src/components/frames/schedule/index.tsx
@@ -1,13 +1,21 @@
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 import Calendar from "./calendar"
 
 import style from "./index.module.css"
 
 export default function ScheduleFrame() {
     const [selectedDate, setSelectedDate] = useState(new Date())
+    const [events, setEvents] = useState([])
+    useEffect(() => {
+        axios.post("https://visoff.ru/api/db/user/find/events", {id:window.user.data._id}).then(data => {
+            setEvents(data.data)
+        })
+    }, [events])
     return (
         <div className={style.main}>
             <Calendar date={[selectedDate, setSelectedDate]} />
+            {events}
         </div>
     )
 }
